9938139b02fdcbc8630896d10fa50fb2212c6537
Rehab <rehab.abotalep@gmail.com>
2021-09-14 19:50:11 +0200

    Merge Sort

--- Sorts/Source.cpp
+++ Sorts/Source.cpp
@@ -58,6 +58,44 @@ void SelectionSort(T A[], int n)
 	}
 }
 
+
+void Merge(int A[], int low, int mid, int high)
+{
+	int i = low, j = mid + 1, k = low;
+	int B[100];
+	while (i <= mid && j <= high)
+	{
+		if (A[i] < A[j])
+			B[k++] = A[i++];
+		else
+			B[k++] = A[j++];
+	}
+	while (i <= mid)
+	{
+		B[k++] = A[i++];
+	}
+
+	while (j <= high)
+	{
+		B[k++] = A[j++];
+	}
+	for (int i = low; i <= high; i++) 
+	{
+		A[i] = B[i];
+	}
+}
+
+void MergeSort(int A[], int low, int high)
+{
+	if (low < high)
+	{
+		int mid = (low + high) / 2;
+		MergeSort(A, low, mid);
+		MergeSort(A, mid + 1, high);
+		Merge(A, low, mid, high);
+	}
+}
+
 int main()
 {	
 	char A[] = { 'b','a','c','d'};
@@ -66,7 +104,7 @@ int main()
 	Print(A, n, "Before sorting");
 
 	SelectionSort(A, n);
-	Print(A, n, "After sorting");
+	Print(A, n, "After Selection sorting");
 	cout << "----------------------------------" << endl;
 	int B[] = { 10,3,5,8,6,9,2,4,1,7 };
 	int s = sizeof(B) / sizeof(B[0]);
@@ -74,7 +112,17 @@ int main()
 	Print(B, s, "Before sorting");
 
 	BubbleSort(B, s);
-	Print(B, s, "After sorting");
+	Print(B, s, "After Buuble sorting");
+
+	int c[] = { 8,2,9,6,5,3,7,4 };
+	int size = sizeof(c) / sizeof(c[0]);
+
+	cout << "----------------------------------" << endl;
+	Print(c, size, "Before sorting");
+	MergeSort(c,0, 7);
+	Print(c, size, "After Merge sorting");
+
+
 
 	return 0;
 }
